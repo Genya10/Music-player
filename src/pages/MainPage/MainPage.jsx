883b93d19988a8fb1a0582e3fe52b529e4f722b5
Jenya <113167825+Genya10@users.noreply.github.com>
2024-01-18 11:00:33 +0200

    create state trackSearch and function search

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import tracks from "../../assets/tracks";
 import style from "./MainPage.module.scss";
 import Track from "../../components/Track/Track";
+import { Input } from "@mui/material";
+
+const search = (query)=>{
+    if(!query){
+        return tracks;
+    }
+    const lowerCaseQuery = query.toLowerCase();
+
+    return tracks.filter((track)=>
+      track.title.toLowerCase().includes(lowerCaseQuery) ||
+      track.artists.toLowerCase().includes(lowerCaseQuery)) 
+}  
 
 const MainPage =()=>{
+    const [trackSearch,setTrackSearch] = useState(tracks);
+
+    const handleChange=(event)=>{
+        setTrackSearch(search(event.target.value));
+    }
+
     return (
         <div className={style.search}>
-            <>Search tracks</>
+         <Input className={style.input} 
+               placeholder="Search tracks"
+               onChange={handleChange}/>
+              
          <div className={style.list}>
-          {tracks.map((track)=>(
+          {trackSearch.map((track)=>(
             <div key={track.id}>
                 <Track {...track}/>
             </div>
@@ -15,9 +37,6 @@ const MainPage =()=>{
          </div>
         </div>
     )
-
- 
 }
-
 
 export default MainPage;
